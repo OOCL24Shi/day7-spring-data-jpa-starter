c16fd9dff8ae97f2087a3d358b7a69b0c484447f
VSHI <36368406@QQ.COM>
2024-11-26 16:39:29 +0800

    [Spring Boot][JPA]: feat, updated test case and implementation to use JPA to create an employee

--- src/main/java/com/oocl/springbootemployee/repository/EmployeeRepository.java
+++ src/main/java/com/oocl/springbootemployee/repository/EmployeeRepository.java
@@ -11,6 +11,7 @@ import java.util.List;
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
         List<Employee> getAllByGender(Gender gender);
+        List<Employee> findAll();
 
 
 }
