ced954e7b315ad783ac8af163ea97d012a8bc078
VSHI <36368406@QQ.COM>
2024-11-26 21:06:03 +0800

    [Spring Boot][JPA]: test, change to JPA for CompanyControllerTest

--- src/main/java/com/oocl/springbootemployee/repository/CompanyRepository.java
+++ src/main/java/com/oocl/springbootemployee/repository/CompanyRepository.java
@@ -6,8 +6,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface CompanyRepository extends JpaRepository<Company, Integer> {
-    List<Company> findAll();
-    Company findById(int id);
-    Company save(Company company);
-    void deleteById(int id);
+//    List<Company> findAll();
+//    Company findById(int id);
+//    Company save(Company company);
+//    void deleteById(int id);
 }
--- src/main/java/com/oocl/springbootemployee/service/CompanyService.java
+++ src/main/java/com/oocl/springbootemployee/service/CompanyService.java
@@ -3,6 +3,9 @@ package com.oocl.springbootemployee.service;
 import com.oocl.springbootemployee.model.Company;
 import com.oocl.springbootemployee.model.Employee;
 import com.oocl.springbootemployee.repository.CompanyInMemoryRepository;
+import com.oocl.springbootemployee.repository.CompanyRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -10,42 +13,49 @@ import java.util.List;
 @Service
 public class CompanyService {
     private final CompanyInMemoryRepository companyInMemoryRepository;
+    private final CompanyRepository companyRepository;
 
-    public CompanyService(CompanyInMemoryRepository companyInMemoryRepository) {
+    public CompanyService(CompanyInMemoryRepository companyInMemoryRepository, CompanyRepository companyRepository) {
         this.companyInMemoryRepository = companyInMemoryRepository;
+        this.companyRepository = companyRepository;
     }
 
     public List<Company> findAll(){
-        return companyInMemoryRepository.findAll();
+        return companyRepository.findAll();
     }
 
     public List<Company> findAll(int pageIndex, int pageSize) {
-        List<Company> companiesInPage = companyInMemoryRepository.getCompaniesByPagination(pageIndex, pageSize);
-        return companiesInPage.stream().toList();
+//        List<Company> companiesInPage = companyRepository.getCompaniesByPagination(pageIndex, pageSize);
+//        return companiesInPage.stream().toList();
+        final Page<Company> companyPage = companyRepository.findAll(PageRequest.of(pageIndex, pageSize));
+        return companyPage.getContent();
     }
 
     public Company findById(Integer id) {
-        return companyInMemoryRepository.findById(id);
+        return companyRepository.findById(id).orElse(null);
     }
 
 
     public List<Employee> getEmployeesByCompanyId(Integer id) {
-        Company company = companyInMemoryRepository.findById(id);
+        Company company = companyRepository.findById(id).orElse(null);
         return company.getEmployees();
     }
 
     public Company create(Company company) {
-        return companyInMemoryRepository.addCompany(company);
+        return companyRepository.save(company);
     }
 
     public Company update(Integer id, Company company) {
-        final var companyNeedToUpdate = companyInMemoryRepository
-                .findById(id);
+        final var companyNeedToUpdate = companyRepository
+                .findById(id).orElse(null);
 
         var nameToUpdate = company.getName() == null ? companyNeedToUpdate.getName() : company.getName();
         var employeesToUpdate = company.getEmployees() == null ? companyNeedToUpdate.getEmployees() : company.getEmployees();
 
         final var companyToUpdate = new Company(id,nameToUpdate,employeesToUpdate);
-        return companyInMemoryRepository.updateCompany(id, companyToUpdate);
+        return companyRepository.save(companyToUpdate);
     }
+
+    public void delete(Integer companyId) {
+        companyRepository.deleteById(companyId);}
 }
